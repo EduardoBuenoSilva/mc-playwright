77595f96e64b4277704da22273c6b9caca7a0e08
Eduardo Bueno <eduardobuenosilva@hotmail.com>
2025-05-13 20:15:58 -0300

    Subindo pipeline

--- tests/Login.spec.js
+++ tests/Login.spec.js
@@ -13,13 +13,13 @@ test('teste com email vazio', async({page}) => {
   await page.goto('https://automationpratice.com.br/login')
   await page.locator('#password').fill('123456')
   await page.getByRole('button', { name: 'login' }).click()
-  await expect(page.getByRole('heading', { name: 'E-mail invalido'})).toBeVisible();
+  await expect(page.getByText('E-mail inválido.')).toBeVisible();
 })
 
 test('teste com senha vazia', async({page}) => {
   await page.goto('https://automationpratice.com.br/login')
   await page.locator('#user').fill('eduardobueno@teste.com')
   await page.getByRole('button', { name: 'login' }).click()
-  await expect(page.getByRole('heading', { name: 'Login realizado'})).toBeVisible();
+  await expect(page.getByText('Senha inválida.')).toBeVisible();
 })
 
